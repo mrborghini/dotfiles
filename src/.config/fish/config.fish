9206a1195afa64bc0b54d17669b8506f063e8fab
mrborghini <61702082+mrborghini@users.noreply.github.com>
2026-08-27 13:13:58 +0200

    feat: add support for !! in fish

--- src/.config/fish/config.fish
+++ src/.config/fish/config.fish
@@ -18,4 +18,30 @@ if status is-interactive
     end
 end
 
+# Previous command with !!
+function bind_bang
+    switch (commandline -t)[-1]
+        case "!"
+            commandline -t -- $history[1]
+            commandline -f repaint
+        case "*"
+            commandline -i !
+    end
+end
+
+function bind_dollar
+    switch (commandline -t)[-1]
+        case "!"
+            commandline -f backward-delete-char history-token-search-backward
+        case "*"
+            commandline -i '$'
+    end
+end
+
+function fish_user_key_bindings
+    bind ! bind_bang
+    bind '$' bind_dollar
+end
+
+
 set -g fish_prompt_pwd_dir_length 0
